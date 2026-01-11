12684dbf6826ce27f34d6009a350f40feeb8e17a
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:22:07 +0000

    Update to schema version v4.57.0

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_site_recovery_vmware_replication_policy" "site_recovery_vmware_replication_policys" {
+  for_each = var.site_recovery_vmware_replication_policys
+
+  application_consistent_snapshot_frequency_in_minutes = each.value.application_consistent_snapshot_frequency_in_minutes
+  name                                                 = each.value.name
+  recovery_point_retention_in_minutes                  = each.value.recovery_point_retention_in_minutes
+  recovery_vault_id                                    = each.value.recovery_vault_id
+}
+
---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "site_recovery_vmware_replication_policys" {
+  description = "All site_recovery_vmware_replication_policy resources"
+  value       = azurerm_site_recovery_vmware_replication_policy.site_recovery_vmware_replication_policys
+}
+
