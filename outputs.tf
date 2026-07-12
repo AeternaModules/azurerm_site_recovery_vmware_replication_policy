a649b19998ab188cd7afb2851d3bd8091d74d3c0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:29 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "site_recovery_vmware_replication_policies" {
-  description = "All site_recovery_vmware_replication_policy resources"
-  value       = azurerm_site_recovery_vmware_replication_policy.site_recovery_vmware_replication_policies
-}
 output "site_recovery_vmware_replication_policies_application_consistent_snapshot_frequency_in_minutes" {
-  description = "List of application_consistent_snapshot_frequency_in_minutes values across all site_recovery_vmware_replication_policies"
-  value       = [for k, v in azurerm_site_recovery_vmware_replication_policy.site_recovery_vmware_replication_policies : v.application_consistent_snapshot_frequency_in_minutes]
+  description = "Map of application_consistent_snapshot_frequency_in_minutes values across all site_recovery_vmware_replication_policies, keyed the same as var.site_recovery_vmware_replication_policies"
+  value       = { for k, v in azurerm_site_recovery_vmware_replication_policy.site_recovery_vmware_replication_policies : k => v.application_consistent_snapshot_frequency_in_minutes }
 }
 output "site_recovery_vmware_replication_policies_name" {
-  description = "List of name values across all site_recovery_vmware_replication_policies"
-  value       = [for k, v in azurerm_site_recovery_vmware_replication_policy.site_recovery_vmware_replication_policies : v.name]
+  description = "Map of name values across all site_recovery_vmware_replication_policies, keyed the same as var.site_recovery_vmware_replication_policies"
+  value       = { for k, v in azurerm_site_recovery_vmware_replication_policy.site_recovery_vmware_replication_policies : k => v.name }
 }
 output "site_recovery_vmware_replication_policies_recovery_point_retention_in_minutes" {
-  description = "List of recovery_point_retention_in_minutes values across all site_recovery_vmware_replication_policies"
-  value       = [for k, v in azurerm_site_recovery_vmware_replication_policy.site_recovery_vmware_replication_policies : v.recovery_point_retention_in_minutes]
+  description = "Map of recovery_point_retention_in_minutes values across all site_recovery_vmware_replication_policies, keyed the same as var.site_recovery_vmware_replication_policies"
+  value       = { for k, v in azurerm_site_recovery_vmware_replication_policy.site_recovery_vmware_replication_policies : k => v.recovery_point_retention_in_minutes }
 }
 output "site_recovery_vmware_replication_policies_recovery_vault_id" {
-  description = "List of recovery_vault_id values across all site_recovery_vmware_replication_policies"
-  value       = [for k, v in azurerm_site_recovery_vmware_replication_policy.site_recovery_vmware_replication_policies : v.recovery_vault_id]
+  description = "Map of recovery_vault_id values across all site_recovery_vmware_replication_policies, keyed the same as var.site_recovery_vmware_replication_policies"
+  value       = { for k, v in azurerm_site_recovery_vmware_replication_policy.site_recovery_vmware_replication_policies : k => v.recovery_vault_id }
 }
 
