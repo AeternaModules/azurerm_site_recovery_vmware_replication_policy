338959da40f23df8780f903f8a06ee8c01258f2f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:05:02 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "site_recovery_vmware_replication_policies_id" {
+  description = "Map of id values across all site_recovery_vmware_replication_policies, keyed the same as var.site_recovery_vmware_replication_policies"
+  value       = { for k, v in azurerm_site_recovery_vmware_replication_policy.site_recovery_vmware_replication_policies : k => v.id }
+}
 output "site_recovery_vmware_replication_policies_application_consistent_snapshot_frequency_in_minutes" {
   description = "Map of application_consistent_snapshot_frequency_in_minutes values across all site_recovery_vmware_replication_policies, keyed the same as var.site_recovery_vmware_replication_policies"
   value       = { for k, v in azurerm_site_recovery_vmware_replication_policy.site_recovery_vmware_replication_policies : k => v.application_consistent_snapshot_frequency_in_minutes }
